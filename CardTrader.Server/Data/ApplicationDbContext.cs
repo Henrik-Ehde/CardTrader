6a1faf35c3d7d5a09438b2395a92b071526464b0
Henrik-Ehde <henrik_ehde@hotmail.com>
2024-05-27 10:44:55 +0200

    Made sure server project is startup project

--- CardTrader.Server/Data/ApplicationDbContext.cs
+++ CardTrader.Server/Data/ApplicationDbContext.cs
@@ -22,9 +22,7 @@ namespace CardTrader.Server.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+
             builder.Entity<Order>()
                 .HasOne(b => b.Buyer)
                 .WithMany(t => t.BuyOrders)
